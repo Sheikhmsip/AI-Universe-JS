5faf5c9c69aba2c66d9c5e5247fe0763e31d44d6
Sheikhmsip <sheikhmsip@gmail.com>
2023-03-03 01:56:44 +0600

    add all AI card with all features, name and date

--- AI-Universe-Hub/script/ai.js
+++ AI-Universe-Hub/script/ai.js
@@ -1,5 +1,38 @@
 const loadAi = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
-    .then(res => res.json())
-    .then(data => console.log(data.data.tools[0]));
+        .then(res => res.json())
+        .then(data => displayAis(data.data.tools));
 }
+
+const displayAis = ais => {
+    // console.log(ais);
+    const aiContainer = document.getElementById('ai-container');
+
+    ais.forEach(ai => {
+        // console.log(ai);
+        const aisDiv = document.createElement('div');
+        aisDiv.classList.add('card');
+        aisDiv.innerHTML = `
+        <div class="card-body bg-base-100 shadow-xl w-[100%] mx-auto py-3 px-5 rounded-lg"">
+            <figure class="px-4 pt-5">
+                <img src="${ai.image}" alt=""/ class="w-[700px]">
+            </figure>
+            <div class="pl-4">
+            <h3 class="card-title mb-2">Features</h3>
+            <div class="pl-2">
+            <p>1. ${ai.features[0]}</p>
+            <p>2. ${ai.features[1]}</p>
+            <p>3. ${ai.features[2] ? ai.features[2] : 'No features available'}</p>
+            </div>
+            </div>
+            
+            
+            
+        </div>
+        `
+       aiContainer.appendChild(aisDiv); 
+    })
+
+}
+
+loadAi();
