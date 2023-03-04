e9cd3630662a1fbfc537c04c7b018d5f9d22c4d3
Sheikhmsip <sheikhmsip@gmail.com>
2023-03-04 13:22:14 +0600

    add see all button to show all data

--- AI-Universe-Hub/script/ai.js
+++ AI-Universe-Hub/script/ai.js
@@ -2,12 +2,29 @@
 const loadAi = () => {
     fetch('https://openapi.programming-hero.com/api/ai/tools')
         .then(res => res.json())
+        .then(data => displayAis(data.data.tools.slice(0, 6)));
+};
+
+const showAllLoadAi = () => {
+    fetch('https://openapi.programming-hero.com/api/ai/tools')
+        .then(res => res.json())
         .then(data => displayAis(data.data.tools));
-}
+};
+
 // Display Ai Card
 const displayAis = ais => {
     // console.log(ais);
     const aiContainer = document.getElementById('ai-container');
+        aiContainer.innerHTML ='';
+        const showAll = document.getElementById('see-all');
+        if (ais.length <= 6) {
+            showAll.classList.remove('hidden');
+        } else {
+            showAll.classList.add('hidden');
+        }
+    // Display only the first 6 cards
+   
+
 
     ais.forEach(ai => {
         // console.log(ai);
@@ -26,14 +43,14 @@ const displayAis = ais => {
                         <p>3. ${ai.features[2] ? ai.features[2] : 'No features available'}</p>
                     </div>
                 </div>
-
+                <div class="bg-slate-300 h-[2px]"></div>
                 <div class="flex justify-between items-center gap-5 ">
                     <div class="pl-4">
                         <h3 class="card-title mb-2">${ai.name}</h3>
                         <p class="pl-1"> <i class="fa-regular fa-calendar-days"></i> ${ai.published_in ? ai.published_in
                 : "No published date"}</p>
                     </div>
-                    <div class="">
+                    <div class="mt-2">
                         
                         <label for="details" onclick="loadAiDetail('${ai.id}')" class="bg-[#FEFF81] h-[40px] w-[40px] rounded-full px-3 py-2 cursor-pointer"><i
                                 class="fa-solid fa-arrow-right text-[#EB5757]"></i>
@@ -56,6 +73,9 @@ const displayAis = ais => {
         `
         aiContainer.appendChild(aisDiv);
     })
+    // showAll.classList.add('hidden');
+
+
 
 }
 
@@ -80,13 +100,13 @@ const displayAiDetais = ai => {
        <h1 class="card-title">${ai.description}</h1>
         <div class="grid grid-cols-3 mt-2 gap-5 max-w-max">
             <div class="bg-red-100 py-1  rounded-lg  ">
-                <h1 class="text-[#03A30A] font-bold text-center">${ai.pricing? ai.pricing[0].price : 'No price'}  ${ai.pricing ? ai.pricing[0].plan : "No plan"}</h1>
+                <h1 class="text-[#03A30A] font-bold text-center">${ai.pricing ? ai.pricing[0].price : 'No price'} <br> ${ai.pricing ? ai.pricing[0].plan : "No plan"}</h1>
             </div>
             <div class="bg-red-100 py-1  rounded-lg">
-                <h1 class="text-[#F28927] font-bold text-center">${ai.pricing?  ai.pricing[1].price : 'No price'} <br> ${ai.pricing ? ai.pricing[1].plan : 'No plan'}</h1>
+                <h1 class="text-[#F28927] font-bold text-center">${ai.pricing ? ai.pricing[1].price : 'No price'} <br> ${ai.pricing ? ai.pricing[1].plan : 'No plan'}</h1>
             </div>
             <div class="bg-red-100 py-1  rounded-lg">
-                <h1 class="text-[#EB5757] font-bold text-center">${ai.pricing ? ai.pricing[2].price : 'No price' } <br> ${ai.pricing ? ai.pricing[2].plan : 'No plan'}</h1>
+                <h1 class="text-[#EB5757] font-bold text-center">${ai.pricing ? ai.pricing[2].price : 'No price'} <br> ${ai.pricing ? ai.pricing[2].plan : 'No plan'}</h1>
             </div>
         </div>
         <div class="grid grid-cols-2 justify-between gap-3 mt-3">
@@ -101,9 +121,9 @@ const displayAiDetais = ai => {
             <div>
                 <h1 class="text-xl font-bold">Integrations</h1>
                 <ul class="list-disc pl-5">
-                    <li>${(ai.integrations !== null) ? ai.integrations[0] : 'No data found'}</li>
-                    <li>${(ai.integrations !== null) ? ai.integrations[1] : 'No data found'}</li>
-                    <li>${(ai.integrations !== null) ? ai.integrations[2] : 'No data found'}</li>
+                    <li>${ai.integrations ? ai.integrations[0] : 'No data found'}</li>
+                    <li>${ai.integrations ? ai.integrations[1] || 'No data found' : 'No data found'}</li>
+                    <li>${ai.integrations ? ai.integrations[2] || 'No data found' : 'No data found'}</li>
                 </ul>
             </div>
         </div>
@@ -115,7 +135,7 @@ const displayAiDetais = ai => {
     <div class="card md:mt-0 lg:mt-0 relative mt-[400px] w-[100%] bg-base-100 shadow-xl max-h-max">
     <figure class="px-5">
     <img src="${ai.image_link[0]}" alt="" class="rounded-xl"/>
-    <h1 class="absolute top-0 right-6 bg-[#EB5757] text-white px-2 rounded-lg">${ai.accuracy.score ? ai.accuracy.score : 'No' } Accuracy</h1>
+    <h1 class="absolute top-0 right-6 bg-[#EB5757] text-white px-2 rounded-lg">${ai.accuracy.score ? ai.accuracy.score : 'No'}% Accuracy</h1>
     </figure>
     <div class="card-body items-center ">
     
@@ -126,5 +146,22 @@ const displayAiDetais = ai => {
     </div>
     `
 }
-// loadAiDetail()
+
+
+// show all card 
+//  add click event to see-all button
+//  showAll.addEventListener('click', (displayAis) => {
+
+//     ais.forEach(ai => {
+
+
+//     });
+//     showAll.classList.add('hidden');
+// });
+
+
+
+
+
+
 loadAi();
